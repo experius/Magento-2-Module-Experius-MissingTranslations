a0fafbcea3ea084a184aa960f329abdcac8bcf14
Ruben Panis <ruben@experius.nl>
2020-03-18 10:35:46 +0100

    [BUGFIX] - Check file exists before processing

--- Module/I18n/Parser/Adapter/Js.php
+++ Module/I18n/Parser/Adapter/Js.php
@@ -22,26 +22,28 @@ class Js extends AbstractAdapter
     {
         $fileHandle = @fopen($this->_file, 'r');
         $lineNumber = 0;
-        while (!feof($fileHandle)) {
-            $lineNumber++;
-            $fileRow = fgets($fileHandle, 4096);
-            $results = [];
-            preg_match_all('/mage\.__\(\s*([\'"])(.*?[^\\\])\1.*?[),]/', $fileRow, $results, PREG_SET_ORDER);
-            for ($i = 0; $i < count($results); $i++) {
-                if (isset($results[$i][2])) {
-                    $quote = $results[$i][1];
-                    $this->_addPhrase($quote . $results[$i][2] . $quote, $lineNumber);
+        if ($fileHandle) {
+            while (!feof($fileHandle)) {
+                $lineNumber++;
+                $fileRow = fgets($fileHandle, 4096);
+                $results = [];
+                preg_match_all('/mage\.__\(\s*([\'"])(.*?[^\\\])\1.*?[),]/', $fileRow, $results, PREG_SET_ORDER);
+                for ($i = 0; $i < count($results); $i++) {
+                    if (isset($results[$i][2])) {
+                        $quote = $results[$i][1];
+                        $this->_addPhrase($quote . $results[$i][2] . $quote, $lineNumber);
+                    }
                 }
-            }
 
-            preg_match_all('/\\$t\(\s*([\'"])(.*?[^\\\])\1.*?[),]/', $fileRow, $results, PREG_SET_ORDER);
-            for ($i = 0; $i < count($results); $i++) {
-                if (isset($results[$i][2])) {
-                    $quote = $results[$i][1];
-                    $this->_addPhrase($quote . $results[$i][2] . $quote, $lineNumber);
+                preg_match_all('/\\$t\(\s*([\'"])(.*?[^\\\])\1.*?[),]/', $fileRow, $results, PREG_SET_ORDER);
+                for ($i = 0; $i < count($results); $i++) {
+                    if (isset($results[$i][2])) {
+                        $quote = $results[$i][1];
+                        $this->_addPhrase($quote . $results[$i][2] . $quote, $lineNumber);
+                    }
                 }
             }
+            fclose($fileHandle);
         }
-        fclose($fileHandle);
     }
 }
