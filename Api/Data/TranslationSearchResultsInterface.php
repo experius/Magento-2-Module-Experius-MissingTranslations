e956e72f1752e4351cec5f1ae4a59aadeb990f8d
Matthijs Breed <matthijs.breed@experius.nl>
2022-09-16 15:03:30 +0200

    [FEATURE][BACI-66]

--- Api/Data/TranslationSearchResultsInterface.php
+++ Api/Data/TranslationSearchResultsInterface.php
@@ -13,12 +13,12 @@ interface TranslationSearchResultsInterface extends \Magento\Framework\Api\Searc
      * Get Translation list.
      * @return \Experius\MissingTranslations\Api\Data\TranslationInterface[]
      */
-    public function getItems();
+    public function getItems(): array;
 
     /**
      * Set key_id list.
      * @param \Experius\MissingTranslations\Api\Data\TranslationInterface[] $items
      * @return $this
      */
-    public function setItems(array $items);
+    public function setItems(array $items): self;
 }
