ab2644a3ed4e48d7ca87ba9b54ace10ffbd5dcd5
Jeffrey leeuw <jeffreyleeuw@happyhorizon.com>
2026-08-20 14:33:47 +0200

    [FEATURE][HD-473] Compatibility php 8.5

--- Console/Command/MissingTranslationsToDatabase.php
+++ Console/Command/MissingTranslationsToDatabase.php
@@ -58,8 +58,7 @@ class MissingTranslationsToDatabase extends Command
     /**
      * {@inheritdoc}
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output): int {
         $this->state->setAreaCode('frontend');
 
         if (!$input->getOption(self::INPUT_KEY_LOCALE)) {
